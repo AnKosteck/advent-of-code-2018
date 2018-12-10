cbac0a738227bb0f5c1fba76a43c115fa55114ba
Andreas Kostecka <kosteckaan@stud.sbg.ac.at>
2018-12-10 15:04:06 +0100

    Update stubs for day5-25

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -1,3 +1,26 @@
+use std::fs::File;
+use std::io::BufReader;
+use std::io::prelude::*;
+
+fn convert_str_to_uint(s: &str) -> u32 {
+    match s.trim().parse() {
+        Ok(num) => num,
+        Err(_) => {
+            println!("Conversion error of {}", s);
+            0
+        },
+    }
+}
+
+fn open_input_file() -> BufReader<File> {
+    let file = File::open("input").unwrap();
+    return BufReader::new(file)
+}
+
 fn main() {
-    println!("Hello, world!");
+    let buf_reader = open_input_file();
+
+    for (nr, line) in buf_reader.lines().enumerate() {
+        let l = line.unwrap();
+    }
 }
